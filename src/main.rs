e7fd8d922f2b57e6ffcdd4714dfa3c6625d203d4
Kevin Jonathan Harnanta <crossmajor99@gmail.com>
2021-12-29 11:44:53 +0700

    Update main.rs
    change variable name

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ use std::net::SocketAddr;
 async fn main() {
     log::log_init();
     let config = configuration::read_config().await;
-    let telegram = Newrelic::new(config.newrelic);
+    let newrelic = Newrelic::new(config.newrelic);
     let addr: SocketAddr = SocketAddr::from((
         "0.0.0.0".parse::<std::net::Ipv4Addr>().unwrap(),
         *config.server.get_port(),
     ));
     let server = axum::Server::bind(&addr)
         .serve(
-            application::build(config.server, telegram)
+            application::build(config.server, newrelic)
                 .into_make_service_with_connect_info::<SocketAddr, _>(),
         )
         .with_graceful_shutdown(application::shutdown_signal());
